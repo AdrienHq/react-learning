073144d6fc97ae3feded9d79977198b54cf50512
AdrienHq <adrienhecq@gmail.com>
2024-02-24 16:40:00 +0100

    feat: event handler / onClick

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,22 +1,28 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
-    const items = ['Un', 'Deux', 'Trois', 'Quatre'];
+  const items = ["Un", "Deux", "Trois", "Quatre"];
 
-    const getListLogic = () => {
-        return items.length === 0 ? <p>No item found</p> : null;
-    }
+  const getListLogic = () => {
+    return items.length === 0 ? <p>No item found</p> : null;
+  };
 
-    return (
-        <>
-            <h1>This is my list</h1>
-            {getListLogic()}
-            <ul className="list-group">
-                {items.map((item) =>
-                    <li key={item}>{item}</li>)
-                }
-            </ul>
-        </>
+  //Event handler
+  const handleClick = (event: MouseEvent) => console.log(event);
 
-    );
+  return (
+    <>
+      <h1>This is my list</h1>
+      {getListLogic()}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li className="list-group-item" key={item} onClick={handleClick}>
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default ListGroup;
