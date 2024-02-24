2c29d6d52cf5b49efcaa1eb65be1df561db28672
AdrienHq <adrienhecq@gmail.com>
2024-02-24 17:57:55 +0100

    feat: feedback to the console after click

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,17 @@ import ListGroup from "./components/ListGroup.tsx";
 function App() {
   let items = ["G2", "FNC", "MDR", "SKT", "KR"];
 
+  const handleSelectedItems = (item: string) => {
+    console.log(item);
+  };
+
   return (
     <div>
-      <ListGroup items={items} heading={"E-sport"} />
+      <ListGroup
+        items={items}
+        heading={"E-sport"}
+        onSelectItem={handleSelectedItems}
+      />
     </div>
   );
 }
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -3,9 +3,11 @@ import { useState } from "react";
 interface ListProps {
   items: string[];
   heading: string;
+
+  onSelectItem: (item: string) => void;
 }
 
-function ListGroup({ items, heading }: ListProps) {
+function ListGroup({ items, heading, onSelectItem }: ListProps) {
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
   const getListLogic = () => {
@@ -30,6 +32,7 @@ function ListGroup({ items, heading }: ListProps) {
             key={item}
             onClick={() => {
               setSelectedIndex(index);
+              onSelectItem(item);
             }}
           >
             {item}
