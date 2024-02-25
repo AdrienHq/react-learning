a78f535296059e9268b62e0c0a340a6f6feade57
AdrienHq <adrienhecq@gmail.com>
2024-02-25 12:40:43 +0100

    feat: make components work together

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 // import ListGroup from "./components/ListGroup.tsx";
+// import Alert from "./components/Alert.tsx";
+
+import Button from "./components/Button.tsx";
 import Alert from "./components/Alert.tsx";
+import { useState } from "react";
 
 function App() {
   // let items = ["G2", "FNC", "MDR", "SKT", "KR"];
@@ -8,12 +12,25 @@ function App() {
   //   console.log(item);
   // };
 
+  const [alertVisible, setAlertVisibility] = useState(false);
+
   return (
     <div>
-      <Alert>
-        My super <h2>alert</h2>
-      </Alert>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>
+          My super alert !{" "}
+        </Alert>
+      )}
+      <Button color={"danger"} onClick={() => setAlertVisibility(true)}>
+        My super button
+      </Button>
     </div>
+
+    // <div>
+    //   <Alert>
+    //     My super <h2>alert</h2>
+    //   </Alert>
+    // </div>
 
     // <div>
     //   <ListGroup
