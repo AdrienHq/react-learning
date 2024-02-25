928c621badcb16da6e3ed574c9aaae7ba176bd74
AdrienHq <adrienhecq@gmail.com>
2024-02-25 12:03:32 +0100

    feat: using children in props

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,27 @@
-import ListGroup from "./components/ListGroup.tsx";
+// import ListGroup from "./components/ListGroup.tsx";
+import Alert from "./components/Alert.tsx";
 
 function App() {
-  let items = ["G2", "FNC", "MDR", "SKT", "KR"];
-
-  const handleSelectedItems = (item: string) => {
-    console.log(item);
-  };
+  // let items = ["G2", "FNC", "MDR", "SKT", "KR"];
+  //
+  // const handleSelectedItems = (item: string) => {
+  //   console.log(item);
+  // };
 
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading={"E-sport"}
-        onSelectItem={handleSelectedItems}
-      />
+      <Alert>
+        My super <h2>alert</h2>
+      </Alert>
     </div>
+
+    // <div>
+    //   <ListGroup
+    //     items={items}
+    //     heading={"E-sport"}
+    //     onSelectItem={handleSelectedItems}
+    //   />
+    // </div>
   );
 }
 
